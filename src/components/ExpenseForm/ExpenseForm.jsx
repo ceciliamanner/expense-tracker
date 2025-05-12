9e0e30f37ef3f6196060618c1f8c4db9c165397e
Cecilia Manner <ceciliamanner@Mac.home>
2025-05-12 21:52:02 +0200

    complete validation

--- src/components/ExpenseForm/ExpenseForm.jsx
+++ src/components/ExpenseForm/ExpenseForm.jsx
@@ -6,15 +6,80 @@ import Button from "../Button/Button";
 
 const ExpenseForm = () => {
     const [expenseInput, setExpenseInput] = useState({
+        category: "",
         title: "", 
         amount: "", 
         date: "", 
-        category: "",
     }); 
+
+    const [error, setError] = useState(null);
+    const [formValidated, setFormValidated] = useState(null)
+
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+
+        setExpenseInput((prevData) => ({ ...prevData, [name]: value }));
+        setError((prevError) => ({ ...prevError, [name]: "" }));
+    };
+
+    const validateInput = () => {
+        const errorObj = {...error};
+        let isValid = true; 
+
+        if(!expenseInput.category){
+            errorObj.category = "Please select a category";
+            isValid = false; 
+        }else {
+            errorObj.category = ""; 
+        }
+
+        if(!expenseInput.title.trim()){
+            errorObj.title = "Title is required";
+            isValid = false; 
+        }else {
+            errorObj.title = ""; 
+        }
+
+        if(!expenseInput.amount){
+            errorObj.amount = "Please enter an amount ";
+            isValid = false; 
+        }else {
+            errorObj.amount = ""; 
+        }
+
+        if(!expenseInput.date){
+            errorObj.date = "Select date";
+            isValid = false; 
+        } else {
+            const inputDate = new Date(expenseInput.date);
+            const today = new Date(); 
+            if(inputDate > today){
+                errorObj.date = "Future dates are not allowed";
+                isValid = false;
+            } else {
+                errorObj.date = "";
+            }
+        }
+
+        setError(errorObj);
+
+        return isValid;
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const formIsValid = validateInput();
+            if(!formIsValid){
+                return;
+            }
+            setFormValidated("Expend has been added");
+    }
+
 
 
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={handleSubmit}>
         <h2 className={styles.formTitle}>Add New Expense</h2>
         <div className={styles.formWrap}>
             <div className={styles.formGroup}>
@@ -23,6 +88,8 @@ const ExpenseForm = () => {
                     type="text"
                     name="category"
                     id="category"
+                    value={expenseInput.category}
+                    onChange={handleChange}
                 >
                     <option value="">Select Category</option>
                     <option value="housing">Housing</option>
@@ -33,6 +100,9 @@ const ExpenseForm = () => {
                     <option value="entertainment">Entertainment</option>
                     <option value="other">Other</option>
                 </select>
+                {error && (
+                    <p className={styles.validationMessage}>{error.category}</p>
+                 )}
             </div>
             <div className={styles.formGroup}>
                 <label htmlFor="title"></label>
@@ -41,7 +111,12 @@ const ExpenseForm = () => {
                     name="title"
                     id="title"
                     placeholder="Title"
+                    value={expenseInput.title}
+                    onChange={handleChange}
                 />
+                 {error && (
+                    <p className={styles.validationMessage}>{error.title}</p>
+                 )}
             </div>
             <div className={styles.formGroup}>
                 <label htmlFor="amount"></label>
@@ -50,7 +125,14 @@ const ExpenseForm = () => {
                     name="amount"
                     id="amount"
                     placeholder="Amount"
+                    min="1"
+                    max="999999"
+                    value={expenseInput.amount}
+                    onChange={handleChange}
                 />
+                {error && (
+                    <p className={styles.validationMessage}>{error.amount}</p>
+                 )}
             </div>
             <div className={styles.formGroup}>
                 <label htmlFor="date"></label>
@@ -58,10 +140,15 @@ const ExpenseForm = () => {
                     type="date"
                     name="date"
                     id="date"
+                    value={expenseInput.date}
+                    onChange={handleChange}
                 />
+                {error && (
+                    <p className={styles.validationMessage}>{error.date}</p>
+                 )}
             </div> 
         </div>
-        <Button className={styles.addButton}>Add Expense</Button>
+        <Button type="submit" className={styles.addButton}>Add Expense</Button>
 
 
     </form>
